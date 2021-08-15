cc7a1905b14468d8a44b5734f230474fe6a3fecb
hacker-prime <88862577+hacker-prime@users.noreply.github.com>
2021-08-15 16:34:09 -0400

    Update config.php

--- Admin/includes/config.php
+++ Admin/includes/config.php
@@ -5,7 +5,7 @@
 	// https://www.php.net/manual/en/timezones.others.php
 	$timezone = date_default_timezone_set("Jamaica");
 	
-	$con = mysqli_connect("localhost", "root", "Archimedes3.14", "u704189932_caquality");
+	$con = mysqli_connect("localhost", "root", "password", "databasename");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
